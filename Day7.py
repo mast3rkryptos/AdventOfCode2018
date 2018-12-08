2b88d375c9c1cc42fbd5d84534af5ef1a74220c7
mast3rkryptos <mast3rkryptos@users.noreply.github.com>
2018-12-08 00:20:20 -0600

    Day 7 complete

--- Day7.py
+++ Day7.py
@@ -1,7 +1,7 @@
 import re
 
-NUMBER_OF_WORKERS = 2
-BASE_TIME = 0
+NUMBER_OF_WORKERS = 5
+BASE_TIME = 60
 
 def main():
     with open("Inputs/Day7") as file:
@@ -22,19 +22,33 @@ def main():
                 break
         print current
 
-        order = current
-        workers = []
+        order = ""
+        workers = {}
+        second = -1
         for i in range(0, NUMBER_OF_WORKERS):
-            workers[i] = 0
-        while len(steps) > 0:
-            for step in sorted(steps.keys()):
-                if current in steps[step]:
-                    steps[step].remove(current)
-            del steps[current]
-            for step in sorted(steps.keys()):
-                if len(steps[step]) == 0:
-                    current = step
-                    order += current
-                    break
-        print order
+            workers[i] = (["", 0])
+        done = False
+        while not done:
+            second += 1
+            for worker in workers.keys():
+                if workers[worker][1] > 0:
+                    workers[worker][1] -= 1
+                else:
+                    for step in sorted(steps.keys()):
+                        if workers[worker][0] in steps[step]:
+                            steps[step].remove(workers[worker][0])
+                    if workers[worker][0] not in order:
+                        order += workers[worker][0]
+                        workers[worker] = ("", 0)
+                        if len(steps) == 0:
+                            done = True
+                            for i in range(0, NUMBER_OF_WORKERS):
+                                done &= workers[i] == ("", 0)
+                    for step in sorted(steps.keys()):
+                        if len(steps[step]) == 0:
+                            workers[worker] = [step, BASE_TIME + (ord(step) - 64) - 1]
+                            del steps[step]
+                            break
+
+        print order, second
 
